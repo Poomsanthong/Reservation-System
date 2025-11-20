4d8a28c5647cf6716210726ab8559f4acc47f648
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-11-20 12:31:56 +0100

    feat: add footer and header components, implement UI elements with Radix UI, and enhance styling
    - Added Footer component for site-wide footer with navigation links and copyright information.
    - Introduced Header component for navigation with booking and admin views, including responsive mobile menu.
    - Created UI components (Avatar, Badge, Button, Calendar, Card, Input, Label, Select) using Radix UI and Tailwind CSS for consistent styling.
    - Implemented utility function for class name merging to simplify conditional styling.
    - Updated package.json to include new dependencies for Radix UI components and date handling.
    - Added components.json for configuration of UI components and styling preferences.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen text-gray-400">
+    <div className="min-h-screen text-gray-400flex flex-col">
       <Header />
-      <div className="container">{children}</div>
+      <div className="container mx-auto flex flex-col items-center justify-center">
+        {children}
+      </div>
       <Footer />
     </div>
   );
