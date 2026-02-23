91a8c0131b241b21e571e8c8c6a0399b0bd31c24
Poomsanthong <thanapoom.santhong10@gmail.com>
2026-02-23 20:00:46 +0100

    feat: update send-confirmation function to use Resend service and adjust event handling
    No Nodemailer

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,8 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "./inngest";
-import { sendconfirmation as sendConfirmationEmail } from "./functions/send-confirmation";
+import { sendconfirmation } from "./functions/send-confirmation";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [sendConfirmationEmail],
+  functions: [sendconfirmation],
 });
--- lib/inngest/functions/send-confirmation.ts
+++ lib/inngest/functions/send-confirmation.ts
@@ -1,36 +1,37 @@
-import nodemailer from "nodemailer";
 import { inngest } from "../inngest";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendconfirmation = inngest.createFunction(
-  { id: "send-confirmation" },
-  { event: "app/reservation.created" }, // Listen for the reservation.created event
-
+  { id: "send-confirmation-email" },
+  { event: "reservation.created" }, // match exactly what you send
   async ({ event }) => {
     const { email, name, reservation_date, reservation_time, partySize } =
       event.data;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+    console.log("FUNCTION TRIGGERED", event.data);
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Your Reservation Confirmation",
-      html: ` <h1>Reservation Confirmed</h1>
-        <p>Hi ${name},</p>
-        <p>Your booking is confirmed:</p>
-        <ul>
-          <li>Date: ${reservation_date}</li>
-          <li>Time: ${reservation_time}</li>
-          <li>Party Size: ${partySize}</li>
-        </ul>
-        `,
-    });
-    return { success: true };
+    try {
+      const result = await resend.emails.send({
+        from: "onboarding@resend.dev", // replace with your verified sender , this is just an example and tested with resend's onboarding email
+        to: email,
+        subject: "Your Reservation Confirmation",
+        html: `<h1>Hello ${name}</h1>
+               <p>Your reservation has been confirmed:</p>
+               <ul>
+                 <li>Date: ${reservation_date}</li>
+                 <li>Time: ${reservation_time}</li>
+                 <li>Party Size: ${partySize}</li>
+               </ul>
+               <p>We look forward to serving you!</p>`,
+      });
+
+      console.log("Email sent result:", result);
+      return { success: true };
+    } catch (err) {
+      console.error("Email failed:", err);
+      throw err;
+    }
   },
 );
