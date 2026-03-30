6c680af86ca8a657c8332bca2a3ba99d287be161
Poomsanthong <thanapoom.santhong10@gmail.com>
2026-03-30 22:51:16 +0200

    feat : when sign in sucessfully , do window.location.href = "/admin"; to full reload to ensure everything is  mount and initialized properly

--- app/(booking)/bookingPage/page.tsx
+++ app/(booking)/bookingPage/page.tsx
@@ -4,13 +4,15 @@ import { getCurrentUserRestaurant } from "@/lib/server/getCurrentUserRestaurant"
 export default async function ClientIndexPage() {
   const { user, restaurant } = await getCurrentUserRestaurant();
 
+  // If no user  is found, redirect to a default booking page (e.g., demo restaurant)
   if (!user) {
-    redirect("/login");
+    redirect("/bookingPage/demo-restaurant");
   }
-
+  // If the restaurant doesn't have a slug, redirect to a default booking page
   if (!restaurant?.slug) {
-    redirect("/login");
+    redirect("/bookingPage/demo-restaurant");
   }
 
+  // Redirect to the restaurant's booking page using its slug if everything is valid
   redirect(`/bookingPage/${restaurant.slug}`);
 }
